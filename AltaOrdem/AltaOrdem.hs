70ad52291479f98aeddeeee6077061c9a909b549
PierreOF <marcos.pierre@upe.br>
2024-05-25 10:57:04 -0300

    add oddQuicksort

--- AltaOrdem/AltaOrdem.hs
+++ AltaOrdem/AltaOrdem.hs
@@ -1,11 +1,13 @@
-quicksort :: [Int] -> [Int]
-quicksort [] = []
-quicksort (x:xs) = quicksort left ++ [x] ++ quicksort right
+oddquicksort :: [Int] -> [Int]
+oddquicksort [] = []
+oddquicksort (x:xs) = oddquicksort left ++ [x] ++ oddquicksort right
     where
         left = [a | a <- xs, a < x && odd a]
         right = [a | a <- xs , a >= x && odd a]
 
+
+
 main :: IO ()
 main = do
     
-    print (quicksort [1,4,3,9,2,7,5])
+    print (oddquicksort [1,4,3,9,2,7,5])
